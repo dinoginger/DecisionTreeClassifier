75b614da954cc29c013de0b1af9ec7e5faf683ed
marco <marcothemars@gmail.com>
2022-02-19 21:20:59 +0200

    split_data - DONE

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Decision Trees Task Code by Roman Mutel & Marko Ruzak
 """
 
 
+
 class Node:
     def __init__(self, X, y, gini):
         self.X = X  # всі 150 квіток -> 150 лістів з чотирма параметрами
@@ -17,10 +18,10 @@ class Node:
 class MyDecisionTreeClassifier:
     
     def __init__(self, max_depth):
-        self.max_depth = max_depth
+        self.max_depth = max_depth  # задавати максимальну висоту дерева.
     
     def gini(self, groups, classes):
-        '''
+        """
         A Gini score gives an idea of how good a split is by how mixed the
         classes are in the two groups created by the split.
         
@@ -28,17 +29,45 @@ class MyDecisionTreeClassifier:
         whereas the worst case split that results in 50/50
         classes in each group result in a Gini score of 0.5
         (for a 2 class problem).
-        '''
-        
+        """
+
         pass
     
     def split_data(self, X, y) -> 'tuple[int, int]':
-        
-        # test all the possible splits in O(N^2)
-        # return index and threshold value
-        
-        pass
-    
+        """
+        Сплітаємо дані в два лісти, за якимось значенням по одному з чотирьох параметрів.
+        Іншими словами, ми ітеруємо наш ліст з елементами, в яких параметрами кожної квітки,
+        (в кожному елементі 4 параметри), та сплітаємо, по суті, за кожним параметром
+        кожної квітки.
+
+        Після цього обчислюємо Gini індекс, та обираємо найкращий варіант спліту.
+
+        :param X: Датасет з параметрами всіх квіток List[list[float, float, float, float]]
+        :param y: Список у якому типи всіх квіток (1, 2 або 3) у відповідності до першого датасету.
+        :return: Повертає індекс і threshold value - індекс це один з чотирьох параметрів,
+        за яким порівнююьтся квітки, а threshold value - значення параметру одної з квіток
+        (тої, за якою і порівнювався (і сплітився кінцево) датасет)
+        """
+            # test all the possible splits in O(N^2)
+            # return index and threshold value
+        best_gini = float("inf")
+
+        for index in range(len(X[0])):
+            for for_value in X:
+                left, right = list(), list()
+                for row in X:
+                    if row[index] <= for_value[index]:
+                        left.append(row)
+                    else:
+                        right.append(row)
+                groups = [left, right]
+
+                gini = self.gini(groups, y)
+                if gini == 0:
+                    return index, for_value[index]
+                if gini < best_gini:
+                    best_gini = gini
+
     def build_tree(self, X, y, depth = 0):
         
         # create a root node
@@ -48,10 +77,11 @@ class MyDecisionTreeClassifier:
         
         pass
     
-    def fit(sefl, X, y):
+    def fit(self, X, y):
         
         # basically wrapper for build tree
-        
+
+        # Sort X here before passing it to data split method!
         pass
     
     def predict(self, X_test):
